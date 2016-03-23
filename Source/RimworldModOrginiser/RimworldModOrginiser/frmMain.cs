93ca13c17c7fe7969b2bd39700446483439f32a8
Jaxxa <jaxxa90@gmail.com>
2016-03-23 19:41:48 +1100

    Initial testing.

---
+++ Source/RimworldModOrginiser/RimworldModOrginiser/frmMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RimworldModOrginiser
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            RimworldModOrginiser.DataObjects.Mod testMod = new RimworldModOrginiser.DataObjects.Mod();
+            testMod.load(@"C:\Games\RimWorldAlpha12dWin\RimWorld914Win\Mods\Core");
+        }
+    }
+}
